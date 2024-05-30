54cd80b4284b4d57d146c634502886bbf830fa92
Pedro <ponsvpedro@gmail.com>
2024-05-30 15:08:16 +0200

    Clarified make_coef_array

--- src_py/boozer.py
+++ src_py/boozer.py
@@ -10,23 +10,24 @@ def make_coef_array(
     coefs, xm, xn, len_th, len_ph, deriv_order=0, deriv_dir=""
 ):
     """Make a 3D array of coefficients with the correct shape to be inverted."""
+    assert deriv_dir in [
+        "th",
+        "ph",
+        "",
+    ], "Invalid derivative dir"  # "" means no derivative
     coef_array = np.zeros(
         (coefs.shape[0], len_th, len_ph), dtype=np.complex128
     )
     xm_mod = np.array(xm) % len_th
     xn_mod = -np.array(xn) % len_ph
     if deriv_order == 0:
-        for idx, ci in enumerate(coefs):
-            np.add.at(coef_array[idx], (xm_mod, xn_mod), ci)
+        coefs_mod = coefs
     elif deriv_order == 1:
         if deriv_dir == "th":
-            for idx, ci in enumerate(coefs):
-                np.add.at(coef_array[idx], (xm_mod, xn_mod), 1j * xm * ci)
+            coefs_mod = 1j * xm * coefs
         elif deriv_dir == "ph":
-            for idx, ci in enumerate(coefs):
-                np.add.at(coef_array[idx], (xm_mod, xn_mod), -1j * xn * ci)
-        else:
-            raise ValueError("Invalid deriv_dir")
+            coefs_mod = -1j * xn * coefs
+    np.add.at(coef_array, np.s_[:, xm_mod, xn_mod], coefs_mod)
     return coef_array
 
 
